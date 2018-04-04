6216d9919f96d15c5a91c2331ef9db1c2385e806
meichengjun <meichengjun@WIN-SO0SSS1UFGU.ad.openearth.community>
2018-04-04 14:29:39 +0800

    test

--- src/main/java/guru/springframework/SpringBootMongodbApplication.java
+++ src/main/java/guru/springframework/SpringBootMongodbApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootMongodbApplication {
 
 	public static void main(String[] args) {
+		//meicj test
 		SpringApplication.run(SpringBootMongodbApplication.class, args);
 	}
 }
